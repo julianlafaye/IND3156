c0c0706467f61c7eb3c204b9014a9d3c61e0c863
julianlafaye <39496078+julianlafaye@users.noreply.github.com>
2018-07-02 16:34:41 -0700

    Update writedata.c
    Trying to figure out how to write to file and then tail the first line of that file to a smaller file

--- writedata.c
+++ writedata.c
@@ -1,2 +1,20 @@
 #include <sensorda.h>
 //writedata.c
+void smalldata
+
+int main () {
+
+FILE* fp;
+use(TempF);
+use(TempC);
+use(Hum);
+
+	fp = fopen("bigdata.txt");
+
+	fprintf( &fp
+
+	smalldata(fp)
+return(0)
+}
+
+void smalldata(fp) {
